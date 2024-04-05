8a30054436284327e6594237be3dfba70bacc046
Justin Wozniak <woz@anl.gov>
2024-04-05 14:37:47 -0500

    Start support for multiple systems

--- models/install-GDRP.sh
+++ models/install-GDRP.sh
@@ -1,33 +1,75 @@
-#!/bin/zsh -f
+#!/bin/bash -norc
 set -eu
 
 # Install GraphDRP in non-container mode
 
+THIS=${0:h:A}
+source $THIS/../workflows/common/sh/utils.sh
+
+signature SYSTEM -- ${*}
+
 which python
 echo
-echo "Install GraphDRP dependencies?  Hit enter or Ctrl-C to cancel."
+echo -n "Install GraphDRP dependencies for $SYSTEM? "
+echo -n "Hit enter or Ctrl-C to cancel."
 read -t 10 _
+echo
 echo "Installing..."
 
-PKGS_PIP=(
-  h5py    # 3.1
-  pyarrow  # 10.0
-  "torch==2.0.1"
-  torch_geometric
-  torch_sparse
-  torch_scatter
-  torch-cluster
-  pubchempy
-  rdkit
-  networkx
+# NEW:
 
-)
+# Requires Python 3.8 for Torch 2.0.0
+# Trying with ALCF Conda, Python 3.10
 
-set -x
-for PKG in $PKGS_PIP
+# ALCF: The ALCF-maintained data science installation
+#       Make a conda clone
+# Anaconda: Plain Anaconda-based install.  Good for Lambda
+
+# "pytorch==1.12.1 torchvision==0.13.1 cudatoolkit=10.2 -c pytorch" # ORIG
+
+# ALCF has pytorch, tensorflow, h5py, matplotlib, networkx, psutil, pyarrow
+case $SYSTEM in
+
+  ALCF)
+    SPECS=(
+      "-c bioconda pubchempy"
+    )
+    ;;
+
+  Anaconda)
+    SPECS=(
+      "-c pytorch pytorch"
+      "-c pyg -c conda-forge pyg" # 2.1.0
+      "-c bioconda pubchempy"
+      # "-c rdkit rdkit" # hangs
+    )
+    ;;
+
+  *) echo "No such SYSTEM='$SYSTEM'"
+     exit 1
+     ;;
+esac
+
+timestamp()
+{
+  # Prevent newline:
+  echo -n $( date +"%Y-%m-%d %H:%M:%S" )
+}
+
+LIST=$SPECS_$SYSTEM
+for SPEC in
 do
-  python -m pip install $PKG
+  timestamp
+  echo "CONDA:" $SPEC
+  echo
+  conda install --yes $=SPEC
+  echo
 done
 
-# candle does not import with scikit-learn 1.2.0
-# /opt/conda/bin/pip install scikit-learn==1.1.3
+timestamp
+echo "PIP: deap"
+pip install deap
+echo
+
+timestamp
+echo "DONE."
