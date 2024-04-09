28dade15d54bec0bf8dcae4a94d15bada09fa5ab
Justin Wozniak <woz@anl.gov>
2024-04-09 13:33:52 -0500

    Update installations on Lambda

--- workflows/common/sh/env-lambda.sh
+++ workflows/common/sh/env-lambda.sh
@@ -8,20 +8,24 @@
 # Everything is installed in here:
 SFW=/homes/woz/Public/sfw
 
-SWIFT=$SFW/swift-t/2022-11-02
-PY=$SFW/Anaconda
+# SWIFT=$SFW/swift-t/2022-11-02
+# SWIFT=$SFW/swift-t/2024-04-05-GDRP
+SWIFT=$SFW/swift-t/2024-04-09-GDRP
+# PY=$SFW/Anaconda
+# PY=$SFW/Anaconda-2024-04-05-GDRP
+PY=$SFW/Miniconda-2024-04-09-GDRP
 # EQPY=$SFW/EQ-Py
 EQR=$SFW/EQ-R
-R=$SFW/R-4.1.0
+# R=$SFW/R-4.1.0
 
 PATH=$SWIFT/stc/bin:$PATH
 PATH=$PY/bin:$PATH
 
 # We only need this for R (including if Swift/T was compiled with R):
-export LD_LIBRARY_PATH=$R/lib/R/lib:${LD_LIBRARY_PATH:-}
+# export LD_LIBRARY_PATH=$R/lib/R/lib:${LD_LIBRARY_PATH:-}
 
 # How to run CANDLE models:
-CANDLE_MODEL_IMPL="app"
+CANDLE_MODEL_IMPL="py"
 
 # PYTHONPATH=$EQPY/src:${PYTHONPATH:-}
 
